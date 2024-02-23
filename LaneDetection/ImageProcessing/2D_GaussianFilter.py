23b2cbec8b119e9e75a7539d1197376033cf8352
홍성민 <landsky1234@naver.com>
2024-02-23 19:36:02 +0900

    Update 2D_GaussianFilter.py

--- LaneDetection/ImageProcessing/2D_GaussianFilter.py
+++ LaneDetection/ImageProcessing/2D_GaussianFilter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 def gaussian_filter2d(width, sigma):
     
@@ -11,7 +12,21 @@ def gaussian_filter2d(width, sigma):
     for x in range(width):
         for y in range(width):
             kernel_2d[x, y] = np.exp(-(distance[x] ** 2 + distance[y] ** 2) / (2 * (sigma ** 2))) / not_exp
+
+    # Image에 전체적인 intensity를 보존하기 위하여 Normalize
+    kernel_2d /= np.sum(kernel_2d)
     
     return kernel_2d
     
-print(gaussian_filter2d(3, 3))
+def apply_gaussian_filter(image, kernel):
+    filter_image = cv2.filter2D(image, -1, kernel)
+    
+    return filter_image
+
+img = cv2.imread('Lane Detection Study\lena.png')
+gaussian_blurImg = apply_gaussian_filter(img, gaussian_filter2d(3, 3))
+
+cv2.imshow('Original Image', img)
+cv2.imshow('Gaussian_blur_Image', gaussian_blurImg)
+cv2.waitKey()
+cv2.destroyAllWindows()
